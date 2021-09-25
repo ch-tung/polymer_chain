f0a1d345ef707266eebde843a9c4f099a6711adc
ch-tung <ch_tung@gapp.nthu.edu.tw>
2021-09-26 00:53:59 +0800

    Quaternion rotation

--- worm_like_micelle/WLM.m
+++ worm_like_micelle/WLM.m
@@ -6,26 +6,26 @@ close all
 unit_C = zeros(3,1); % coordinate of C atoms in each unit
 
 % Degree of polymerization
-DP_backbone = 200; 
+DP_backbone = 100; 
 
 % Chain stiffness
-a_backbone = 5;
+a_backbone = 2;
 
 % Unit persistence
 % lambda = 2.5234667395;
 lambda = 1;
 
-[lc_backbone,Cc_backbone,O_backbone,n_backbone] = WLM_chain(DP_backbone,a_backbone,lambda,unit_C);
+[lc_backbone,Cc_backbone,O_backbone,n_backbone] = chain(DP_backbone,a_backbone,lambda,unit_C);
 
-% test_plot
-pC2=plot3(Cc_backbone(1,:),Cc_backbone(2,:),Cc_backbone(3,:), 'o-','Color','#000000','MarkerSize',12,'MarkerFaceColor','#303030');
+% plot backbone
+pC_b=plot3(Cc_backbone(1,:),Cc_backbone(2,:),Cc_backbone(3,:), 'o-','Color','#303030','MarkerSize',8,'MarkerFaceColor','#303030','LineWidth',2);
 
 set (gcf,'Position',[0,0,800,800])
 axis equal
 
 %% branch
 inteval = 1;
-DP_branch = 20;
+DP_branch = 1;
 a_branch = 10;
 
 N_branch = floor(DP_backbone/inteval);
@@ -35,14 +35,14 @@ Cc_branch_r = zeros(3,DP_branch,N_branch);
 hold on
 
 for i = 1:N_branch
-Cc_branch(:,:,i) = WLM_chain(DP_branch,a_branch,lambda,unit_C);
+Cc_branch(:,:,i) = chain(DP_branch,a_branch,lambda,unit_C);
 Cc_branch(:,:,i) = Cc_branch(:,:,i) - Cc_branch(:,1,i);
 
 t = pi/2;
-p = pi*2/6*i;
+p = pi*2/7*i;
 
-R_branch = [1 0 0; 0 cos(p) sin(p); 0 -sin(p) cos(p)]; % Roll
-Y_branch = [cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1]; % Yaw
+Y_branch = [cos(t) -sin(t) 0; sin(t) cos(t) 0; 0 0 1];
+R_branch = [1 0 0; 0 cos(p) sin(p); 0 -sin(p) cos(p)];
 
 Cc_branch_r(:,:,i) = O_backbone(:,:,i*inteval)*R_branch*Y_branch*Cc_branch(:,:,i) + lc_backbone(:,i*inteval);
 plot3(Cc_branch_r(1,:,i),Cc_branch_r(2,:,i),Cc_branch_r(3,:,i), 'o-','Color','#002030','MarkerSize',6,'MarkerFaceColor','#0080C0');
--- worm_like_micelle/chain.m
+++ worm_like_micelle/chain.m
@@ -4,28 +4,51 @@ function [lc,Cc,O,n] = chain(DP,a,lambda,unit_C)
 n=zeros(3,DP);
 l=zeros(3,DP);
 lc=zeros(3,DP);
-P=zeros(3,3);
-Y=zeros(3,3);
+B=zeros(3,3);
+C=zeros(3,3);
+D=zeros(3,3);
+R=zeros(3,3);
 O=zeros(3,3,DP);
 for i=1:DP
     if i==1
-        n(:,i) = [0;0;1];
+        n(:,i) = [1;0;0];
         l(:,i) = n(:,i);
-        P(:,:,i) = eye(3);
-        Y(:,:,i) = eye(3);
+        B(:,:,i) = eye(3);
+        C(:,:,i) = eye(3);
+        D(:,:,i) = eye(3);
         R(:,:,i) = eye(3);
-        O(:,:,i) = P(:,:,i)*Y(:,:,i);
+        O(:,:,i) = R(:,:,i);
     else
-        theta_y = 2*(rand-0.5)*pi/a/2;
-        theta_z = 2*(rand-0.5)*pi/a/2;
-        phi = 2*(rand-0.5)*pi/5;
-
-        P(:,:) = [cos(theta_y) 0 sin(theta_y); 0 1 0; -sin(theta_y) 0 cos(theta_y)];
-        Y(:,:) = [cos(theta_z) -sin(theta_z) 0; sin(theta_z) cos(theta_z) 0; 0 0 1];
-        R(:,:) = [1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi)];
         
-        O(:,:,i) = P*Y*O(:,:,i-1);
-        n(:,i) = P*Y*n(:,i-1);
+%         % xyz convention
+%         psi = 0; % Roll angle
+%         theta = 2*(rand-0.5)*pi/a/2; % Pitch angle 
+%         phi = 2*(rand-0.5)*pi/a/2; % Yaw angle
+% 
+%         D(:,:) = [cos(phi) sin(phi) 0; -sin(phi) cos(phi) 0; 0 0 1]; % Yaw
+%         C(:,:) = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)]; % Pitch
+%         B(:,:) = [1 0 0; 0 cos(psi) sin(psi); 0 -sin(psi) cos(psi)]; % Roll
+%         
+%         R = B*C*D;
+        
+        % quaternion
+        phi_q = 2*(rand-0.5)*pi/2;
+        theta_q = (rand-0.5)*pi/a;
+        
+        vq = O(:,2,i-1)*cos(phi_q) + O(:,3,i-1)*sin(phi_q);
+        qr = cos(theta_q/2);
+        qi = vq(1)*sin(theta_q/2);
+        qj = vq(2)*sin(theta_q/2);
+        qk = vq(3)*sin(theta_q/2);
+        
+        Rq = [1-2*(qj^2+qk^2) 2*(qi*qj-qk*qr) 2*(qi*qk+qj*qr);...
+              2*(qi*qj+qk*qr) 1-2*(qi^2+qk^2) 2*(qj*qk-qi*qr);...
+              2*(qi*qk-qj*qr) 2*(qj*qk+qi*qr) 1-2*(qj^2+qk^2)];
+        
+        R = Rq;
+                
+        O(:,:,i) = R*O(:,:,i-1);
+        n(:,i) = R*n(:,i-1);
         l(:,i) = l(:,i-1) + n(:,i);
         
     end
