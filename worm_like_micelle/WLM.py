2257df6b3cec922ab7e4bf4c2748cc040206edb4
ch-tung <ch_tung@gapp.nthu.edu.tw>
2021-10-03 13:46:28 +0800

    self avoiding

--- worm_like_micelle/WLM.py
+++ worm_like_micelle/WLM.py
@@ -29,6 +29,11 @@ def rotation(O,a):
     qi = vq[0]*np.sin(theta_q/2);
     qj = vq[1]*np.sin(theta_q/2);
     qk = vq[2]*np.sin(theta_q/2);
+    nq = np.sqrt(qr**2 + qi**2 + qj**2 + qk**2)
+    qr = qr/nq
+    qi = qi/nq
+    qj = qj/nq
+    qk = qk/nq
     
     Rq = np.array([[1-2*(qj**2+qk**2), 2*(qi*qj+qk*qr), 2*(qi*qk-qj*qr)],
                     [2*(qi*qj-qk*qr), 1-2*(qi**2+qk**2), 2*(qj*qk+qi*qr)],
@@ -38,7 +43,7 @@ def rotation(O,a):
     R = Rq[:,:,0]
     return R
     
-def chain_Rayleigh(DP, a, lambda_seg, unit_C, apply_SA=True, d_exc=1):
+def chain_Rayleigh(DP, a, lambda_seg, unit_C, apply_SA=True, d_exc=0.5):
        
     n = np.zeros((3,DP))
     l = np.zeros((3,DP))
@@ -81,7 +86,9 @@ def chain_Rayleigh(DP, a, lambda_seg, unit_C, apply_SA=True, d_exc=1):
             R = rotation(O[:,:,i-1],a)
             
             O[:,:,i] = R@O[:,:,i-1]
-            n[:,i] = R@n[:,i-1]
+            O[:,:,i] = O[:,:,i]/np.sqrt(np.sum(O[:,:,i]**2,axis=0))
+            n[:,i] = O[:,1,i].reshape((3))
+            # n[:,i] = n[:,i]/np.sqrt(np.sum(n[:,i]**2))
             l[:,i] = l[:,i-1] + n[:,i]
             
             #%% check self avoiding
@@ -93,7 +100,9 @@ def chain_Rayleigh(DP, a, lambda_seg, unit_C, apply_SA=True, d_exc=1):
                         R = rotation(O[:,:,i-1],a)
             
                         O[:,:,i] = R@O[:,:,i-1]
-                        n[:,i] = R@n[:,i-1]
+                        O[:,:,i] = O[:,:,i]/np.sqrt(np.sum(O[:,:,i]**2,axis=0))
+                        n[:,i] = O[:,1,i].reshape((3))
+                        # n[:,i] = n[:,i]/np.sqrt(np.sum(n[:,i]**2))
                         l[:,i] = l[:,i-1] + n[:,i]
                     else:
                         break
@@ -136,22 +145,38 @@ fig = plt.figure(figsize=(6, 6),dpi=192)
 ax = fig.add_subplot(projection='3d')
 
 ax.plot(Cc_backbone[0,:],Cc_backbone[1,:],Cc_backbone[2,:], 
-           '-', color='#D00000', linewidth=2, alpha = 0.75)
+        '-', color='#D00000', linewidth=2, alpha = 0.75)
 # ax.plot(Cc_backbone[0,:],Cc_backbone[1,:],Cc_backbone[2,:], 
-#             'o', markeredgecolor='#800000', markerfacecolor='#D00000')
+#         'o', markeredgecolor='#800000', markerfacecolor='#D00000')
+
+ax.plot(Cc_backbone[0,0],Cc_backbone[1,0],Cc_backbone[2,0], 
+            'o', markeredgecolor='#800000', markerfacecolor='#D00000')
+ax.plot(Cc_backbone[0,-1],Cc_backbone[1,-1],Cc_backbone[2,-1], 
+            'o', markeredgecolor='#800000', markerfacecolor='#D00000')
 
 CM = np.mean(Cc_backbone,axis=1)
+CT = np.array([np.max(Cc_backbone[0,:])+np.min(Cc_backbone[0,:]),
+               np.max(Cc_backbone[1,:])+np.min(Cc_backbone[1,:]),
+               np.max(Cc_backbone[2,:])+np.min(Cc_backbone[2,:])])/2
 d_box = np.max([np.max(Cc_backbone[0,:])-np.min(Cc_backbone[0,:]),
                 np.max(Cc_backbone[1,:])-np.min(Cc_backbone[1,:]),
                 np.max(Cc_backbone[2,:])-np.min(Cc_backbone[2,:])])
 
 #ax.axis('off')
-ax.set_xlim([CM[0]-d_box/2, CM[0]+d_box/2])
-ax.set_ylim([CM[1]-d_box/2, CM[1]+d_box/2])
-ax.set_zlim([CM[2]-d_box/2, CM[2]+d_box/2])
+ax.set_xlim([CT[0]-d_box/2, CT[0]+d_box/2])
+ax.set_ylim([CT[1]-d_box/2, CT[1]+d_box/2])
+ax.set_zlim([CT[2]-d_box/2, CT[2]+d_box/2])
 ax.set_box_aspect([1,1,1])
-ax.set_xticklabels([])
-ax.set_yticklabels([])
-ax.set_zticklabels([])
+# ax.set_xticklabels([])
+# ax.set_yticklabels([])
+# ax.set_zticklabels([])
 
 plt.show()
+
+#%%
+# fig2 = plt.figure(figsize=(6, 6),dpi=192)
+# ax2 = fig2.add_subplot()
+
+# ax2.plot(np.arange(DP_backbone), np.sum(n_backbone**2,axis=0),'-')
+# ax2.set_yscale('log')
+# ax2.set_ylim([0.5, 2])
