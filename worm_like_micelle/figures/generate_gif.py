6a134d9e02f5464749e79983dec32d41a60733ee
ch-tung <ch_tung@gapp.nthu.edu.tw>
2021-10-09 22:22:39 +0800

    figures

--- worm_like_micelle/figures/generate_gif.py
+++ worm_like_micelle/figures/generate_gif.py
@@ -3,10 +3,10 @@ from IPython import display
 import glob
 import imageio
 
-anim_file = './WLM_a2_DP100.gif'
+anim_file = './chain/chain.gif'
 
 with imageio.get_writer(anim_file, mode='I') as writer:
-    filenames = glob.glob('./WLM_a2_DP100_*.png')
+    filenames = glob.glob('./chain/chain_test_*.png')
     filenames = sorted(filenames)
     for filename in filenames:
         image = imageio.imread(filename)
