375696e46dc7049483f2468282c05ce33e0cee47
ch-tung <ch_tung@gapp.nthu.edu.tw>
2021-11-03 00:01:35 +0800

    adjust d_exc according to the persistence length (R=b/10)

---
+++ worm_like_micelle/batchscatter/scatter_sfr_woSA.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  4 11:55:37 2021
+Scattering function
+@author: CHTUNG
+"""
+import sys
+sys.modules[__name__].__dict__.clear()
+
+import numpy as np
+import numpy.matlib
+import time
+from WLM import WLChain
+import threading
+
+#%% test
+# backbone
+# Coordinate of C atoms in each unit
+# unit_C = load('b_c.dat')';
+unit_C = np.zeros((3,1)) # coordinate of C atoms in each unit
+
+# Degree of polymerization
+N_backbone = 10000
+
+def scattering_loop(n_q,n_chain,chain01):
+    qq = np.zeros(n_q)
+    S_q_j = np.zeros(n_q)
+    for i in np.arange(n_chain):
+
+        # tStart = time.time()
+        chain01.apply_SA = 0
+        chain01.chain()
+        #chain01.ring(n_harmonics=40,sigma=10)
+        #chain01.ring_q()
+        # tEnd = time.time()
+        # print("\'chain\' cost %f sec" % (tEnd - tStart))
+        
+        # N = chain01.N
+        # chain_box = chain01.box
+        
+        # tStart = time.time()
+        # chain01.scatter_grid(n_grid=n_q*2)
+        # chain01.scatter_grid_direct(n_q=len(qq),n_grid=256,box_size=np.max(chain_box[1,:]-chain_box[0,:])+1) 
+        chain01.scatter_direct(n_q=np.size(qq),n_merge=1)
+        S_q_j = S_q_j + chain01.S_q
+        # tEnd = time.time()
+        # print("\'scatter\' cost %f sec" % (tEnd - tStart))
+        
+    S_q_j = S_q_j/n_chain
+        
+    return qq, S_q_j
+
+def job(j):
+    # Chain stiffness
+    a_backbone = n_j**((j+1)/2)
+    # a_backbone = 2e3
+	
+    # Unit persistence
+    lambda_backbone = 1
+    
+    # call class
+    chain01 = WLChain(N_backbone,a_backbone,lambda_backbone,unit_C)
+    chain01.d_exc = 1
+    
+    n_q = 64 
+    n_chain = 100
+
+    # tStart_loop = time.time()
+    qq, S_q_j = scattering_loop(n_q,n_chain,chain01)
+    # tEnd_loop = time.time()
+    # print("\'loop\' cost %f sec" % (tEnd_loop - tStart_loop))
+    
+    S_q[:,j] = S_q_j
+    
+a = np.zeros(10)
+S_q = np.zeros((64,10))
+qq = 2*np.pi/(np.logspace(1,5,64))
+n_j = 10
+
+class MyThread(threading.Thread):
+    def __init__(self, j):
+        threading.Thread.__init__(self)
+        self.j = j
+    
+    def run(self):
+        job(self.j)
+
+threads = []
+tStart = time.time()
+
+for j in range(n_j):
+    a_backbone = 2*n_j**((j+1)/2)
+    # a_backbone = 2e3
+    a[j] = a_backbone
+    
+    # job(j)
+    # threads.append(threading.Thread(target = job, args = (j,)))
+    threads.append(MyThread(j))
+    threads[j].start()
+    
+for j in range(n_j):
+    threads[j].join()
+
+qq = 2*np.pi/(np.logspace(1,5,64))
+
+tEnd = time.time()
+print("it cost %f sec" % (tEnd - tStart))
+from scipy.io import savemat
+filename = 'scatter_chain_sfr_woSA.mat'
+mdic = {'S_q':S_q, 'a':a, 'qq':qq}
+savemat(filename, mdic)
---
+++ worm_like_micelle/chain.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  9 20:54:38 2021
+Ring
+@author: CHTUNG
+"""
+import sys
+sys.modules[__name__].__dict__.clear()
+
+import numpy as np
+import numpy.matlib
+#from scipy.io import loadmat
+#from scipy.io import savemat
+import matplotlib.pyplot as plt
+from WLM import WLChain
+import time
+
+# Degree of polymerization
+N_backbone = 10000
+
+# Chain stiffness
+a_backbone = 2e1
+
+# Unit persistence
+lambda_backbone = 1
+
+# call class
+chain01 = WLChain(N_backbone,a_backbone,lambda_backbone)
+chain01.d_exc = 1
+
+plt.close('all')
+for i in range(1):
+    tStart = time.time()
+    chain01.d_exc = chain01.a*0.1*2
+    chain01.apply_SA = 1
+    # chain01.chain()
+    chain01.chain_fix_val_free_rot()
+    tEnd = time.time()
+    print("\'chain\' cost %f sec" % (tEnd - tStart))
+    
+    filename_ring = './figures/ring/ring_test_{:d}.png'.format(i+1)
+    chain01.plot(filename=filename_ring, show_axes=0, save=0, end=0)
+
