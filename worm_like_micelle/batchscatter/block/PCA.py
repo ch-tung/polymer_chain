9c1deb5622cff90df9bace94f1dd475fd80c2324
ch-tung <ch_tung@gapp.nthu.edu.tw>
2021-12-06 00:21:44 +0800

    calculate scattering function of block co-polymer

--- worm_like_micelle/batchscatter/block/PCA.py
+++ worm_like_micelle/batchscatter/block/PCA.py
@@ -18,6 +18,7 @@ filename = 'scatter_chain_block.mat'
 scatter_dict = loadmat(filename)
 S_q  = scatter_dict['S_q']
 p = scatter_dict['p']
+qq = scatter_dict['qq']
 set_ra = sorted(set(p[0]))
 set_a2 = sorted(set(p[1]))
 set_f = sorted(set(p[2]))
@@ -25,7 +26,7 @@ set_f = sorted(set(p[2]))
 #%% SVD
 F = S_q
 F = np.log(S_q)
-F = F - np.mean(F,axis=0)
+# F = F - np.mean(F,axis=0)
 F = F
 U, S, Vh = np.linalg.svd(F)
 
@@ -41,12 +42,35 @@ index_p = index_p_a2
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(projection='3d')
 ax.scatter(score_F[index_p,0], score_F[index_p,1], score_F[index_p,2], 
-           c=np.log(p[0][index_p]),
+           c=np.log(p[2][index_p]),
            s=10)
 ax.view_init(elev=25, azim=-135)
-ax.set_xlabel('lv[0]')
-ax.set_ylabel('lv[1]')
-ax.set_zlabel('lv[2]')
+ax.set_xlabel('SVD[0]')
+ax.set_ylabel('SVD[1]')
+ax.set_zlabel('SVD[2]')
 
 plt.show()
 
+#%% plot basis
+fig = plt.figure(figsize=(6, 6))
+ax_basis = fig.add_subplot()
+
+n_basis = 4
+x = np.linspace(0.0, 1.0, n_basis)
+color = plt.get_cmap('viridis')(x)
+
+for i in range(n_basis):
+    ax_basis.plot(qq.T,U[:,i], color = color[i])
+
+ax_basis.set_xscale('log')
+ax_basis.set_xlabel('Q')
+ax_basis.set_ylabel('score')
+
+#%% plot variance
+fig = plt.figure(figsize=(6, 6))
+ax_var = fig.add_subplot()
+ax_var.plot(np.arange(len(S)),S)
+
+ax_var.set_yscale('log')
+ax_var.set_xlabel('rank')
+ax_var.set_ylabel(r'$\Sigma$')
