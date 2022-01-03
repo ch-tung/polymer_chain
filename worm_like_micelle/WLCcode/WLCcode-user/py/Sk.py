ef2b819f8fee3f47636ae0b1a54b65b1d7f91e7a
ch-tung <ch_tung@gapp.nthu.edu.tw>
2022-01-03 14:46:10 +0800

    python code calculating WLC scattering function

---
+++ worm_like_micelle/WLCcode/WLCcode-user/py/Sk.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 11:51:50 2022
+WLC scattering functions based on the MATLAB code modified by Guan-Rong Huang Oct. 16 2019
+@author: CHTUNG
+"""
+
+import numpy as np
+import f_Sk
+
+def Sk(q,L,b):
+    p1 = 4.12
+    p2 = 4.42
+    p1short = 5.36
+    p2short = 5.62
+    q0 = 3.1
+    
+    #
+    len_q = q.shape[0]
+    i=np.arange(len_q)
+    q0short = np.zeros(len_q)
+    q0short[i] = np.max((1.9*b/np.sqrt(f_Sk.Rgsquareshort(q[i],L,b)),3))
+    
+    #
+    if L/b > 10:
+        C = 3.06/(L/b)**0.44
+    else:
+        C = 1
+
+    #%% S
+    S = np.zeros(len_q)
+    for i in range(len_q):
+        if L > 4*b: # Longer Chains
+            if q[i]*b <= 3.1:
+                # Modified by Yun on Oct. 15,
+                Sexvmodify = f_Sk.Sexvnew(q, L, b)
+                
+                S[i] = (Sexvmodify[i] + C*(4/15 + 7/(15*f_Sk.u(q[i],L,b)) - 
+                                           (11/15 + 7/(15*f_Sk.u(q[i],L,b)))*np.exp(-f_Sk.u(q[i],L,b)))*(b/L))
+                # End of modification
+                
+            else: #q[i]*b > 3.1
+                S[i] = (f_Sk.a1long(q[i], L, b, p1, p2, q0)/((q[i]*b)**(p1)) + 
+                        f_Sk.a2long(q[i], L, b, p1, p2, q0)/((q[i]*b)**(p2)) +
+                        np.pi/(q[i]*L))
+                
+        else: # L <= 4*b Shorter Chains
+        # Modified by Guan-Rong Huang Oct. 16 2019
+            if q[i]*b <= max((1.9*b/np.sqrt(f_Sk.Rgsquareshort(q[i],L,b)),3)):
+                if q[i]*b<=0.01:
+                    S[i] = 1 - f_Sk.Rgsquareshort(q[i],L,b)*(q[i]**(2))/3
+                else:
+                    S[i] = f_Sk.Sdebye1(q[i],L,b)
+                    
+            else: #q[i]*b > max((1.9*b/np.sqrt(f_Sk.Rgsquareshort(q[i],L,b)),3))
+                S[i] = (f_Sk.a1short(q[i],L,b,p1short,p2short,q0short[i])/((q[i]*b)**(p1short)) + 
+                        f_Sk.a2short(q[i],L,b,p1short,p2short,q0short[i])/((q[i]*b)**(p2short)) +
+                        np.pi/(q[i]*L))
+                
+    return S
+                
---
+++ worm_like_micelle/WLCcode/WLCcode-user/py/test_Sk.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  3 11:53:50 2022
+
+@author: CHTUNG
+"""
+
+import numpy as np
+import numpy.matlib
+from scipy.io import loadmat
+# from scipy.io import savemat
+# import matplotlib
+import matplotlib.pyplot as plt
+# from scipy import interpolate
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+
+import time
+tStart = time.time()
+
+#%% load
+# load mat files
+# grep shape
+filename = '../scatter_chain_fvfr_SA.mat'
+scatter_dict = loadmat(filename)
+qq_max = 1
+qq = scatter_dict['qq'][0,:]
+S_q_0  = scatter_dict['S_q'][qq<qq_max,:]
+
+qq = (np.logspace(-4,0,65))
+
+import Sk
+
+q = qq
+L = 5000
+b = 1000
+S_q_th = Sk.Sk(q,L,b)
+
+plt.close('all')
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot()
+ax.plot(qq,S_q_th)
+ax.plot(qq[qq*b>3.1],S_q_th[qq*b>3.1])
+
+ax.set_xscale('log')
+ax.set_yscale('log')
