7bc9e5304c48145df572a869dcc70447c5aaf95c
ch-tung <ch_tung@gapp.nthu.edu.tw>
2022-01-04 00:12:18 +0800

    plot

--- worm_like_micelle/batchscatter/block/parametrized/plotscatter.py
+++ worm_like_micelle/batchscatter/block/parametrized/plotscatter.py
@@ -69,7 +69,7 @@ S_q_0  = scatter_dict['S_q'][qq<qq_max,:]
 p_0 = scatter_dict['p']
 
 
-n_mat = 10
+n_mat = 11
 S_q = np.zeros((np.shape(S_q_0)[0],np.shape(S_q_0)[1]*n_mat))
 p = np.zeros((np.shape(p_0)[0],np.shape(p_0)[1]*n_mat))
 for i in range(n_mat):
@@ -82,11 +82,23 @@ set_ra = sorted(set(p[0]))
 set_a2 = sorted(set(p[1]))
 set_f = sorted(set(p[2]))
 
+# fix the deviation high q limit
+import Sk
+q = qq
+L = 1000
+b = L*2
+S_q_rod = Sk.S_rod(q,L)
+
+for i in range(S_q.shape[1]):
+    S_q_i = S_q[:,i]
+    S_q_i[S_q_i<S_q_rod] = S_q_rod[S_q_i<S_q_rod]
+    S_q[:,i] = S_q_i
+
 qq = qq[qq<qq_max]
 
 Plot.qq = scatter_dict['qq'][0,:]
 
-Plot.n_plot = len(set_f)
+Plot.n_plot = 11
 Plot.n_color = Plot.n_plot
 Plot.plot_setup()
 Plot.ax.set_xlim([5e-4, 2e-1])
@@ -102,11 +114,40 @@ Plot.ax.set_xlim([5e-4, 2e-1])
 # Plot.ax.plot(qq_sfr.T,S_q_sfr[:,[0]],'--',color = [0,0,0])
 # Plot.ax.plot(qq_sfr.T,S_q_sfr[:,[3]],'-.',color = [0,0,0])
 
-#%%
-index_p_a2 = (p[1] == set_a2[0])
-index_p_ra = (p[0] == set_ra[9])
-index_p_f = (p[2] == set_f[8])
-index_p = index_p_f&index_p_a2
+#%%prepare input
+# property to be presented 0
+p_0 = p[0]
+pc_0 = (p_0-np.nanmin(p_0))/(np.nanmax(p_0)-np.nanmin(p_0))
+
+# property to be presented 1
+p_1 = p[1]/1000
+pc_1 = (p_1-np.nanmin(p_1))/(np.nanmax(p_1)-np.nanmin(p_1))
+
+# property to be presented 2
+p_2 = p[2]
+pc_2 = (p_2-np.nanmin(p_2))/(np.nanmax(p_2)-np.nanmin(p_2))
+
+# load stats
+filename_stats = 'stats_block.mat'
+filename_parameters = 'parameters_block.mat'
+stats_dict = loadmat(filename_stats)
+parameters_dict = loadmat(filename_parameters)
+
+stats = stats_dict['statistics']
+p_m1 = stats[:,0]
+p_m2 = stats[:,1]
+p_m3 = stats[:,2]
+
+set_m1 = sorted(set(p_m1))
+set_m2 = sorted(set(p_m2))
+set_m3 = sorted(set(p_m3))
+
+index_p_m1 = (p_m1 == set_m1[10])
+index_p_m2 = (p_m2 == set_m2[10])
+index_p_m3 = (p_m3 == set_m3[5])
+index_p = index_p_m3&index_p_m2
+# index_p = np.arange(len(p[1])) # all datapoints, bool
+
 Plot.S_q = S_q[:,index_p]
 Plot.p = p[:,index_p]
 Plot.plot('-')
